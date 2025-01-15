cef21c4af10241d635ac5bdb11c2351474c2ef31
Corentin Dubois <corentindubois22@gmail.com>
2025-01-15 14:15:01 -0600

    Modified the layout and added a transition effect when new runes are dropping.

--- src/RuneList.jsx
+++ src/RuneList.jsx
@@ -4,8 +4,20 @@ import PropType from 'prop-types';
 
 function RuneList({ currentRunes }) {
 
+  function changeRunesListLength(currentRunes) {
+    if (currentRunes.length === 1 ) {
+      return "one";
+    } else if (currentRunes.length === 2) {
+      return "two";
+    } else if (currentRunes.length === 3) {
+      return "three";
+    } else {
+      return;
+    }
+  }
+
   return (
-    <div className="runes-list">
+    <div className={`runes-list ${changeRunesListLength(currentRunes)}`}>	
       {currentRunes.map((rune, index) =>(
         <li className="runes-list-item" key={index}>
           <Rune
